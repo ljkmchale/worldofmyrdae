be81f8bfad8b225c6924314b4e98d9f32303bae4
ljkmchale <larry.m.mchale@gmail.com>
2026-04-23 09:16:11 -0400

    Ignore local backups and remove them from git

--- backups/full-snapshot.20260422_110219/electron/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge } = require('electron');
-
-contextBridge.exposeInMainWorld('desktopApp', {
-    isElectron: true,
-    platform: process.platform
-});
--- backups/full-snapshot.20260422_110219/js/cities/farview.js
+++
@@ -1,9 +0,0 @@
-window.CITY_MAPS_REGISTRY = window.CITY_MAPS_REGISTRY || {};
-window.CITY_MAPS_REGISTRY["farview"] = {
-  id: "farview",
-  name: "Farview",
-  image: "images/cities/farview/farview.png",
-  previewImage: "images/cities/farview/farview.png",
-  pins: [],
-  namedLabels: [],
-};
